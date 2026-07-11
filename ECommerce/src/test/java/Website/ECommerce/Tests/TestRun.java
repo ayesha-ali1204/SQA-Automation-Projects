55010b9299d11123242698c7ea7264eacb7a8423
ayesha <ayeshaali4876@gmail.com>
2026-07-11 10:52:24 +0500

    Login Test File

--- ECommerce/src/test/java/Website/ECommerce/Tests/TestRun.java
+++ ECommerce/src/test/java/Website/ECommerce/Tests/TestRun.java
@@ -1,26 +1,16 @@
 package Website.ECommerce.Tests;
 
 import java.io.IOException;
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import Website.ECommerce.PageObjects.CheckoutPage;
 import Website.ECommerce.PageObjects.ConfirmationPage;
-import Website.ECommerce.PageObjects.LoginApplication;
 import Website.ECommerce.PageObjects.ProductCatalogue;
 import Website.ECommerce.TestComponents.BaseTest;
 
-//import POM_Framework.LoginApplication;
-
 public class TestRun extends BaseTest{
-
 	@Test
 	public void SubmitOrder() throws IOException {
 
@@ -31,8 +21,7 @@ public class TestRun extends BaseTest{
 		
 //		Return Type of LaunchApplication method is "LoginApplication" class
 //		LaunchApplication method is @BeforeMethod/ it will run everytime before running any test
-
-
+//		
 //		return type of Login method is Checkout class - Object
 		ProductCatalogue catalogue = loginApp.Login(email, password);
 
